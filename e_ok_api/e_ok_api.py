1b43cd6efbf502181d467ad2b5bb926fb1b9bbbc
EsuS <31799121+dotEsuS@users.noreply.github.com>
2019-01-01 13:38:24 +0100

    Update e_ok_api.py
    Add docstrings with information
    Rename function for readability
    Add two new functions

--- e_ok_api/e_ok_api.py
+++ e_ok_api/e_ok_api.py
@@ -35,32 +35,68 @@ class TimeEOK:
         # return the open session object for future handling
         return self.session.post(START_URL + 'user/login/', data=raw, cookies=session_cookies, headers=headers)
 
-    def get_userid(self):
+    def get_employee_id(self):
         html = self.session.get(START_URL).text
         regex = '(?<=var mdwID = )[^;]+'
         match = re.findall(regex, html)
         return_id = demjson.decode(match[0])
         return str(return_id)
 
-    def get_todays_logs(self, userid):
-        todays_logs = self.session.get(START_URL + 'api/TijdLog/GetTijdLogsVanMedewerker/' + userid)
+    def get_todays_logs(self, employee_id):
+        """
+        :param employee_id: Required: ID of the employee
+        :return: List containing dictionary objects with log information for today.
+        """
+        todays_logs = self.session.get(START_URL + 'api/TijdLog/GetTijdLogsVanMedewerker/' + employee_id)
         # return(todays_logs.text)
         # Convert the javascript object to Python object
         return demjson.decode(todays_logs.text)
 
-    def get_todays_workhours(self, userid):
-        todays_workhours = self.session.get(START_URL + 'api/TijdLog/GetWerkurenMedewerker/' + userid)
+    def get_todays_workhours(self, employee_id):
+        """
+        :param employee_id: Required: ID of the employee
+        :return: String with total amount of workhours that should be performed today.
+        """
+        todays_workhours = self.session.get(START_URL + 'api/TijdLog/GetWerkurenMedewerker/' + employee_id)
         return todays_workhours.text
 
-    def get_remaining_workhours(self, userid):
-        remaining_workhours = self.session.get(START_URL + 'api/TijdLog/GetNegPresterenMedewerker/' + userid)
-        return remaining_workhours.text
+    def get_todays_remaining_workhours(self, employee_id):
+        """
+        :param employee_id: Required: ID of the employee
+        :return: String with remaining amount of workhours for today.
+        """
+        todays_remaining_workhours = self.session.get(
+            START_URL + 'api/TijdLog/GetNegPresterenMedewerker/' + employee_id)
+        return todays_remaining_workhours.text
 
-    def get_statistics(self, userid):
+    def get_statistics(self, employee_id):
         # TODO Implement method to get_statistics
         return
 
-    def get_organisation_counter_settings(self, userid):
-        counter_settings = self.session.get(START_URL + 'api/TijdLog/orgCon_counterSettings?mdwid=' + userid)
+    def get_organisation_counter_settings(self, employee_id):
+        """
+        :param employee_id: Required: ID of the employee
+        :return: Dictionary containing information about the settings for the timer.
+        """
+        counter_settings = self.session.get(START_URL + 'api/TijdLog/orgCon_counterSettings?mdwid=' + employee_id)
         return demjson.decode(counter_settings.text)
+
+    def get_is_location_mandatory(self, employee_id):
+        """
+        :param employee_id: Required: ID of the employee
+        :return: Dictionary containing information about location setting
+        """
+        is_location_mandatory = self.session.get(
+            START_URL + 'api/TijdLog/GetIsLocatieVerplicht/?id=' + employee_id + '&apiVersie=2')
+        return demjson.decode(is_location_mandatory.text)
+
+    def get_summarised_timelogs_from_month(self, employee_id, month_year):
+        """
+        :param employee_id: Required: ID of the employee
+        :param month_year: Required: String formatted like 'december 2018'
+        :return: List containing dictionary objects with summarised timelog information about the selected month.
+        """
+        timelogs_from_month = self.session.get(
+            START_URL + 'api/TijdLog/GetTijdLogsVanMaand/?userID=' + employee_id + '&maand=' + month_year + '&apiVersie=2')
+        return demjson.decode(timelogs_from_month.text)
 
